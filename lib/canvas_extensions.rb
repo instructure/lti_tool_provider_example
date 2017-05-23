59a2ae78a1c5bd04fe31c0b911a4d8db14965862
Andrew Butterfield <abutterfield@instructure.com>
2017-05-23 13:57:09 -0700

    Alphabetize placements
    Change-Id: Ide3fc568ce4ee5e56aedebfdcec1a6b17f367dc0

--- lib/canvas_extensions.rb
+++ lib/canvas_extensions.rb
@@ -3,7 +3,7 @@ module CanvasExtensions
   PLACEMENTS = [
     {key: :account_navigation, name: 'Account Navigation'},
     {key: :assignment_menu, name: 'Assignment Menu', message: [:content_item_selection, :basic_lti_request]},
-    {key: :similarity_detection, name: 'Similarity Detection', message: [:basic_lti_request]},
+    {key: :assignment_selection, name: 'Assignment Selection', message: [:content_item_selection_request, :basic_lti_request]},
     {key: :collaboration, name: 'Collaboration', message: [:content_item_selection_request, :basic_lti_request]},
     {key: :course_assignments_menu, name: 'Course Assignments Menu', message: [:basic_lti_request]},
     {key: :course_home_sub_navigation, name: 'Course Home Sub Navigation'},
@@ -14,14 +14,14 @@ module CanvasExtensions
     {key: :file_menu, name: 'File Menu', message: [:content_item_selection, :basic_lti_request]},
     {key: :global_navigation, name: 'Global Navigation'},
     {key: :homework_submission, name: 'Homework Submission', message: [:content_item_selection_request, :basic_lti_request]},
+    {key: :link_selection, name: 'Link Selection', message: [:content_item_selection_request, :basic_lti_request]},
     {key: :migration_selection, name: 'Migration Selection', message: [:content_item_selection_request, :basic_lti_request]},
     {key: :module_menu, name: 'Module Menu', message: [:content_item_selection, :basic_lti_request]},
+    {key: :post_grades, name: 'Post Grades'},
     {key: :quiz_menu, name: 'Quiz Menu', message: [:content_item_selection, :basic_lti_request]},
-    {key: :user_navigation, name: 'User Navigation'},
-    {key: :assignment_selection, name: 'Assignment Selection', message: [:content_item_selection_request, :basic_lti_request]},
-    {key: :link_selection, name: 'Link Selection', message: [:content_item_selection_request, :basic_lti_request]},
-    {key: :wiki_page_menu, name: 'Wiki Page Menu', message: [:content_item_selection, :basic_lti_request]},
+    {key: :similarity_detection, name: 'Similarity Detection', message: [:basic_lti_request]},
     {key: :tool_configuration, name: 'Tool Configuration'},
-    {key: :post_grades, name: 'Post Grades'}
+    {key: :user_navigation, name: 'User Navigation'},
+    {key: :wiki_page_menu, name: 'Wiki Page Menu', message: [:content_item_selection, :basic_lti_request]},
   ]
 end
